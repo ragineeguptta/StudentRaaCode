908a3f471cc070ab0ff979a821cd36ef4ab320f9
Raginee <raginee.gupta@hirdhav.com>
2025-05-25 19:26:20 +0530

    8. CRUD Operation part 2

--- StudentRaaCode.Web/Controllers/StudentController.cs
+++ StudentRaaCode.Web/Controllers/StudentController.cs
@@ -7,12 +7,79 @@ namespace StudentRaaCode.Web.Controllers
     public class StudentController : Controller
     {
         private ApplicationDbContext _context;
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            List<Student> students = new List<Student>();
-            students.Add(new Student { Id = 1, Name = "Raginee" });
-            students.Add(new Student { Id = 2, Name = "Gunjan" });
+            var students = _context.Students.ToList();
+
+            //List<Student> students = new List<Student>();
+            //students.Add(new Student { Id = 1, Name = "Raginee" });
+            //students.Add(new Student { Id = 2, Name = "Gunjan" });
             return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Update(student);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
