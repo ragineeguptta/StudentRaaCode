2a0b3a2dd4cd6d240feb281caf39caf808011ad8
Raginee <raginee.gupta@hirdhav.com>
2025-05-25 16:08:17 +0530

    7. CRUD Operation on Single Model using Model First Apprach Entity Framework Core-1

--- StudentRaaCode.Web/Controllers/StudentController.cs
+++ StudentRaaCode.Web/Controllers/StudentController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentRaaCode.Web.Data;
 using StudentRaaCode.Web.Models;
 
 namespace StudentRaaCode.Web.Controllers
 {
     public class StudentController : Controller
     {
+        private ApplicationDbContext _context;
         public IActionResult Index()
         {
             List<Student> students = new List<Student>();
---
+++ StudentRaaCode.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentRaaCode.Web.Models;
+
+namespace StudentRaaCode.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; } = null!;
+    }
+}
